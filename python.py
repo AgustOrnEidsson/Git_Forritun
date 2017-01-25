53a6012bf8f37901b01420107871cea889aff9e7
AgustOrnEidsson <agustei295@nemi.tskoli.is>
2017-01-25 08:29:36 +0000

    Dæmi 1

--- python.py
+++ python.py
@@ -1,1 +1,10 @@
 #Git verkefni forritun
+
+#dæmi 1
+
+tala1=int(input("Sláðu inn tölu "))
+tala2=int(input("Sláðu inn tölu "))
+tala3=tala1+tala2
+tala4=tala1*tala2
+print("Lagðar samana=",tala3)
+print("Margfaldaðar=",tala4)
